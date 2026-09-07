c71a1341a2217b2f0869e7f9b1d7579291726f8b
rendyyy <ariqahmadf@gmail.com>
2026-09-07 12:38:51 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,78 @@
+import pygame
+import asyncio  # 1. Required for WASM/browser compatibility
+
+points = [
+    (350, 290), 
+    (390, 290), 
+    (390, 215), 
+    (450, 215), 
+    (450, 140), 
+    (380, 140), 
+    (310, 140), 
+    (310, 50), 
+    (170, 50), 
+    (50, 50), 
+    (50, 140), 
+    (50, 215),
+    (170, 215), 
+    (170, 140), 
+    (240, 140), 
+    (240, 215),
+    (310, 215),
+    (310, 290),
+]
+
+async def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 800))
+    clock = pygame.time.Clock()
+    running = True
+
+     # initialize maze
+    maze = pygame.image.load("maze.png")
+    maze_rect = maze.get_rect()
+    maze_rect.topleft = (10, 10)
+
+    # initialize blinky
+    class Ghost:
+        pos = pygame.Vector2(0, 0)
+        sprites = []
+        next_point = 0
+    blinky = Ghost()
+    # starting pos
+    blinky.pos = pygame.Vector2(320, 325)
+    for i in range(4):
+        blinky.sprites.append(pygame.image.load(f"blinky/blinky_{i}.png"))
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("black")
+
+        # draw maze
+        scaled = pygame.transform.scale(maze, (maze.get_width()*3, maze.get_height()*3))
+        screen.blit(scaled, maze_rect)
+
+        # draw ghosts
+        scaled = pygame.transform.scale(blinky.sprites[0], (blinky.sprites[0].get_width()*3, blinky.sprites[0].get_height()*3))
+        rect = scaled.get_rect()
+        rect.x = blinky.pos.x
+        rect.y = blinky.pos.y
+        screen.blit(scaled, rect)
+
+         # draw all points (debugging)
+        for p in range(len(points)):
+            col = p/len(points) * 255 + 10
+            pygame.draw.circle(screen, (col, 0, col), points[p], 5, 2)
+
+        pygame.display.flip()
+        print(pygame.mouse.get_pos())
+        
+        clock.tick(60)
+        # 3. Yield control to the browser loop so it doesn't freeze
+        await asyncio.sleep(0) 
+
+if __name__ == "__main__":
+    asyncio.run(main())
