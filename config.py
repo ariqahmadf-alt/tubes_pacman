76df75d1b7dbdb8b975b82544b8bcfd9805fa61a
Retropaint <darkglasses1122@gmail.com>
2026-09-08 16:50:25 +0700

    - replaced hardcoded points with point generator based on image - new config.py file for configurations - moved maze and point stuff to maze.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# Type of maze to use.
+# 0 - Simple
+# 1 - Bigger
+# 2 - Original Pac-man layout
+maze_type = 2
+
+# Scale of the maze (and rest of the game).
+maze_scale = 20
+
+# How fast ghosts move.
+# Can be set to 0 to freeze them.
+ghost_speed = 3
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
-import asyncio  # 1. Required for WASM/browser compatibility
+import asyncio
+from copy import deepcopy
+import config
+import maze
+
+path = [(0, 0), (0, 1), (0, 2), (0, 3)]
 
 
 class Ghost:
@@ -10,43 +15,49 @@ class Ghost:
     dir = 0
 
     # next point index - which point this ghost is currently going to
-    npi = 0
+    npi = (1, 1)
 
     def ai(self):
         # current point
-        point = points[self.npi]
+        point = deepcopy(maze.points[self.npi[1]][self.npi[0]])
+        point.pos *= config.maze_scale
+        point.pos.x += config.maze_scale / 2
+        point.pos.y += config.maze_scale / 2
 
         # move ghost to their npi
         #
         # when moving, always check against ghost and point pos to ensure
         # that ghost lands on the point instead of overshooting
-        speed = 0
-        if self.pos.x > point[0]:
-            self.pos.x -= min(speed, abs(point[0] - self.pos.x))
+        if self.pos.x > point.pos.x:
+            self.pos.x -= min(config.ghost_speed, abs(point.pos.x - self.pos.x))
             self.dir = 1
-        elif self.pos.x < point[0]:
-            self.pos.x += min(speed, abs(point[0] - self.pos.x))
+        elif self.pos.x < point.pos.x:
+            self.pos.x += min(config.ghost_speed, abs(point.pos.x - self.pos.x))
             self.dir = 0
 
-        if self.pos.y > point[1]:
-            self.pos.y -= min(speed, abs(point[1] - self.pos.y))
+        if self.pos.y > point.pos.y:
+            self.pos.y -= min(config.ghost_speed, abs(point.pos.y - self.pos.y))
             self.dir = 2
-        elif self.pos.y < point[1]:
-            self.pos.y += min(speed, abs(point[1] - self.pos.y))
+        elif self.pos.y < point.pos.y:
+            self.pos.y += min(config.ghost_speed, abs(point.pos.y - self.pos.y))
             self.dir = 3
 
         # move to next point if ghost is close to its current
-        if abs(point[0] - self.pos.x) + abs(point[1] - self.pos.y) < 1:
-            self.npi += 1
-            print(self.npi)
+        if abs(point.pos.x - self.pos.x) + abs(point.pos.y - self.pos.y) < 1:
+            if not maze.points[point.right[1]][point.right[0]].wall:
+                self.npi = (point.right[0], point.right[1])
+            elif not maze.points[point.bottom[1]][point.bottom[0]].wall:
+                self.npi = (point.bottom[0], point.bottom[1])
+            elif not maze.points[point.left[1]][point.left[0]].wall:
+                self.npi = (point.left[0], point.left[1])
 
     def draw(self, screen):
         sprite = self.sprites[self.dir]
         scaled = pygame.transform.scale(
             sprite,
             (
-                sprite.get_width() * 3,
-                sprite.get_height() * 3,
+                sprite.get_width() * config.maze_scale / 15,
+                sprite.get_height() * config.maze_scale / 15,
             ),
         )
         rect = scaled.get_rect()
@@ -55,106 +66,22 @@ class Ghost:
         screen.blit(scaled, rect)
 
 
-points = [
-    # top-left
-    (310, 290),
-    (310, 215),
-    (240, 215),
-    (240, 140),
-    (310, 140),
-    (310, 50),
-    (170, 50),
-    (50, 50),
-    (50, 140),
-    (50, 215),
-    (170, 215),
-    (170, 355),
-    (170, 140),  # middle
-    (240, 355),
-    (240, 290),
-    #
-    # top-right
-    (385, 290),
-    (385, 215),
-    (450, 215),
-    (450, 140),
-    (385, 140),
-    (385, 50),
-    (450, 50),
-    (530, 50),
-    (640, 50),
-    (640, 140),
-    (640, 215),
-    (530, 215),
-    (530, 355),
-    (530, 140),  # middle
-    (450, 355),
-    (450, 290),
-    #
-    # bottom left
-    (240, 430),  # fruit left
-    (240, 500),
-    (170, 500),
-    (50, 500),
-    (50, 575),
-    (90, 575),
-    (90, 645),
-    (50, 645),
-    (50, 710),
-    (310, 710),
-    (310, 645),
-    (240, 645),
-    (240, 575),
-    (300, 575),
-    (300, 500),
-    (170, 575),  # middle
-    (170, 645),  # middle
-    #
-    # bottom right
-    (450, 430),  # fruit right
-    (450, 500),
-    (385, 500),
-    (385, 575),
-    (450, 575),
-    (450, 645),
-    (385, 645),
-    (385, 710),
-    (640, 710),
-    (640, 645),
-    (595, 645),
-    (595, 575),
-    (640, 575),
-    (640, 500),
-    (530, 500),
-    (530, 575), # middle
-    (530, 645), # middle
-]
-
-# initialize maze
-maze = pygame.image.load("maze.png")
-maze_rect = maze.get_rect()
-maze_rect.topleft = (10, 10)
-
-
 # initialize blinky
 blinky = Ghost()
-blinky.pos = pygame.Vector2(350, 290)
+blinky.pos = pygame.Vector2(maze.points[0][0].pos.x, maze.points[0][0].pos.y)
 for i in range(4):
     blinky.sprites.append(pygame.image.load(f"blinky/blinky_{i}.png"))
 
 
 def draw_points(screen):
-    # draw all points (debugging)
-    interval = 15
-    for p in range(len(points)):
-        if p < interval * 1:
-            col = min(p / interval * 1 * 255 + 50, 255)
-            pygame.draw.circle(screen, (col, 0, 0), points[p], 5, 2)
-        elif p < interval * 2:
-            col = min(p / interval * 2 * 255 + 10, 255)
-            pygame.draw.circle(screen, (col, col, 0), points[p], 5, 2)
-        else:
-            pygame.draw.circle(screen, (col, col, col), points[p], 5, 2)
+    for y in range(len(maze.points)):
+        for x in range(len(maze.points[y])):
+            if maze.points[y][x].wall:
+                continue
+            pos = maze.points[y][x].pos * config.maze_scale
+            pos.x += config.maze_scale / 2
+            pos.y += config.maze_scale / 2
+            pygame.draw.circle(screen, (0, 0, 150), pos, 5, 2)
 
 
 async def main():
@@ -172,17 +99,20 @@ async def main():
 
         # draw maze
         scaled = pygame.transform.scale(
-            maze, (maze.get_width() * 3, maze.get_height() * 3)
+            maze.img,
+            (
+                maze.img.get_width() * config.maze_scale,
+                maze.img.get_height() * config.maze_scale,
+            ),
         )
-        screen.blit(scaled, maze_rect)
+        screen.blit(scaled, maze.rect)
+        draw_points(screen)
 
         blinky.ai()
         blinky.draw(screen)
 
-        draw_points(screen)
-
         pygame.display.flip()
-        print(pygame.mouse.get_pos())
+        # print(pygame.mouse.get_pos())
 
         clock.tick(60)
         # 3. Yield control to the browser loop so it doesn't freeze
